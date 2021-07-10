1fd7c0371a51d7b52826abf3b2bdb3a7536cae3f
Abhishek Saral <abhisheksaral@gmail.com>
2021-07-10 15:22:29 -0700

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Abhishek Saral on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
